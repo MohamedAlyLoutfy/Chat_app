d0d2a99094da6721cf035d7697637ffc30897931
Mohamed Ali <mohamedloutfy97@gmail.com>
2021-08-05 14:26:03 +0200

    connecting firebase to flutter

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,16 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 class ChatScreen  extends StatelessWidget {
+  
   
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
+      body:  ListView.builder(
         itemCount: 10,
         itemBuilder: (ctx,index)=>Container(
           padding: EdgeInsets.all(8),
@@ -13,6 +18,39 @@ class ChatScreen  extends StatelessWidget {
           
           ),
         
+      ),
+      floatingActionButton: FloatingActionButton(
+        child:Icon(Icons.add),
+        onPressed: (){
+         // Firebase.initializeApp();
+        FirebaseFirestore.instance
+         .collection('chats/NKZhi2vFepIJBSMiYyyg/messages')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+        querySnapshot.docs.forEach((doc) {
+            print(doc["text"]);
+        });
+    });
+          
+          
+          // FirebaseFirestore.instance.
+          // collection('chats/NKZhi2vFepIJBSMiYyyg/messages')
+          // .snapshots()
+          // .listen((data) { 
+            //data.documents.forEach((document){
+             //  print (document['text']);
+            //
+            //
+            //});
+            
+          //   
+          // });
+          
+          
+
+        },
+      
+      
       ),
     );
   }
